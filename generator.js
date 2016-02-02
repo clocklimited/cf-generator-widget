514eb712be3e82e0bd4916fcdf53c20c4db619df
Matthew Harrison-Jones <matthojo@gmail.com>
2016-02-02 13:15:36 +0000

    > Request the Parts repo data and filter Parts names

--- generator.js
+++ generator.js
@@ -1,16 +1,12 @@
 module.exports = ProjectWidgetGenerator
 
 var kickoff = require('kickoff')
-  , inquirer = require('inquirer')
   , Generator = kickoff.Generator
   , generate = kickoff.generate
   , extend = require('lodash.assign')
-  , slugg = require('slugg')
-  , availableTemplates =
-      [
-        'Custom'
-      , 'Tabbed List'
-      ]
+  , request = require('request')
+  , allPartsData = null
+  , partNames = []
 
 function required (value) {
   return !!value
@@ -20,7 +16,43 @@ function ProjectWidgetGenerator () {
   Generator.call(this)
 }
 
+function getParts (cb) {
+
+  var partsUrl = 'https://api.github.com/repos/clocklimited/parts-new/git/trees/master?recursive=1'
+  request(
+    {
+      headers: {
+        'User-Agent': 'Project Widget Generator'
+      }
+    , url: partsUrl
+    , method: 'GET'
+    }
+  , function (err, res, body) {
+    if (!err) {
+      allPartsData = JSON.parse(body)
+      allPartsData.tree.forEach(function (item) {
+        if (item.type === 'tree') {
+          var partCheck = item.path.match(/^Parts\/(\w+)$/)
+          if (partCheck) {
+            partNames.push(partCheck[1])
+            return true
+          }
+        }
+      })
+      console.log(partNames)
+      cb(null)
+    } else {
+      throw err
+    }
+  })
+}
+
 ProjectWidgetGenerator.prototype = Object.create(Generator.prototype)
+
+ProjectWidgetGenerator.prototype.createConfig = function (userInput) {
+  var config = {}
+  return extend({}, userInput, config)
+}
 
 ProjectWidgetGenerator.prototype.prompts =
   [ { name: 'title'
@@ -34,10 +66,13 @@ ProjectWidgetGenerator.prototype.prompts =
   , { type: 'list'
     , name: 'template'
     , message: 'Which widget template would you like to use?'
-    , choices: availableTemplates
+    , choices: ['Custom']
     }
   ]
 
 ProjectWidgetGenerator.prototype._generate = function (path, config, cb) {
-  generate(__dirname, path, config, cb)
+  getParts(function (err) {
+    if (err) throw err
+    generate(__dirname, path, config, cb)
+  })
 }
